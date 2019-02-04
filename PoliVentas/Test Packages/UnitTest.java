ae0434c22971dd05ee1f6dffeb3c4dae844ee5ba
davnm5 <josdanei@espol.edu.ec>
2019-02-03 21:21:33 -0500

    [UPD] Pruebas unitarias restantes

--- PoliVentas/Test Packages/UnitTest.java
+++ PoliVentas/Test Packages/UnitTest.java
@@ -85,4 +85,18 @@ public class UnitTest extends TestCase {
                 
     }
     
+    public static void testobtenerProductos(){
+        ArrayList<Producto> productos=ProductoDAO.obtener_productos();
+        Assert.assertSame(productos.size(),14);
+                
+    }
+    
+    public static void testverificarProducto(){
+        ArrayList<Producto> productos=ProductoDAO.verificar_producto("Iphone 8");
+        Assert.assertSame(productos.size(),29);
+                
+    }
+    
+    
+    
 }
